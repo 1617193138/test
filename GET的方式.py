571be5054ec9f8870d55bca414e53590b2edc5a8
纪淑元 <33241743+1617193138@users.noreply.github.com>
2017-11-15 09:08:39 +0800

    Update GET的方式.py

--- GET的方式.py
+++ GET的方式.py
@@ -2,8 +2,8 @@ import urllib
 import urllib2
  
 values = {}
-values['username'] = "805043442@qq.com"
-values['password'] = "wy980913.."
+values['username'] = "813818992@qq.com"
+values['password'] = "813818992+jsy."
 data = urllib.urlencode(values) 
 url = "http://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
 request = urllib2.Request(url,data)
